8059a519c46f433a456debbc76667e13e60f3e09
Spencer Everett <spencerweverett@gmail.com>
2024-10-21 19:39:24 -0700

    Added a `download_files()` method that allows for concurrent downloads

--- terminus/downloads.py
+++ terminus/downloads.py
@@ -4,42 +4,7 @@ This file contains utility functions for downloading files & extracting archives
 
 from pathlib import Path
 import requests
-
-# def download_file(url: str, output_dir: Path, vb: bool = False) -> Path:
-#     '''
-#     Downloads a file from a given URL and saves it to the specified output 
-#     directory
-    
-#     Parameters
-#     ----------
-#     url : str
-#         The URL of the file to download.
-#     output_dir : Path
-#         The directory where the file should be saved.
-#     vb: bool
-#         Whether to print verbose output to the terminal
-
-#     Returns
-#     -------
-#     Path
-#         The local path of the downloaded file.
-#     '''
-
-#     local_filename = output_dir / Path(url).name
-
-#     print(f'Downloading to {local_filename}...')
-    
-#     if local_filename.exists():
-#         print(f'File already exists and will be skipped: {local_filename}')
-#         return local_filename
-
-#     with requests.get(url, stream=True) as r:
-#         r.raise_for_status()
-#         with local_filename.open('wb') as f:
-#             for chunk in r.iter_content(chunk_size=8192):
-#                 f.write(chunk)
-
-#     return local_filename
+from concurrent.futures import ThreadPoolExecutor
 
 def download_file(url: str, output_dir: Path, vb: bool = True) -> Path:
     '''
@@ -97,3 +62,40 @@ def download_file(url: str, output_dir: Path, vb: bool = True) -> Path:
             )
 
     return local_filename
+
+def download_files(
+        file_urls: list[str],
+        output_dir: str | Path,
+        max_workers: int = 4,
+        vb: bool = True
+        ) -> None:
+    '''
+    Download a list of files concurrently to the specified output directory.
+
+    Parameters
+    ----------
+    file_list : list[str | Path]
+        A list of file names to download.
+    output_dir : str, Path
+        The directory where the files should be saved.
+    max_workers : int
+        The maximum number of concurrent downloads, if necessary
+    vb: bool
+        Whether to print download status updates to the terminal. Defaults to True.
+    '''
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        future_to_url = {
+            executor.submit(
+                download_file, url, output_dir, vb=vb
+                ): url for url in file_urls
+            }
+        for future in future_to_url:
+            try:
+                future.result()
+                if vb is True:
+                    print(f'Downloaded: {future_to_url[future]}')
+            except Exception as e:
+                print(f'Failed to download {future_to_url[future]}: {e}')
+
+    return
